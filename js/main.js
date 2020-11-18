0787b93e8a8256de83b36df70e27a9843a19fd82
Islam Fathi <46162891+islam-fathi@users.noreply.github.com>
2020-11-18 16:26:33 +0200

    last day work

--- js/main.js
+++ js/main.js
@@ -46,52 +46,7 @@ $(document).ready(function() {
             }, 1200);
     });
 
-    // datatables
-    var customersData = [
-        ["1234", ['<img src="images/user.jpg" width="30px" class="profile_pic rounded-circle mx-1" style="border: 2px solid #fff;">',
-                'أسامة عسكر'
-            ],
-            ['<img src="icons/kwait.svg" width="30px" class="profile_pic mx-1">', "+201234567890"],
-            "info@website.com",
-            '<i class="fas fa-check text-success"></i>',
-            '<button class="small text-white btn-sm btn-primary border-0 mx-1 mb-1 " style="background-color: #6788FF;"><i class="fas fa-pen mx-1"></i>تعديل</button><button class="small text-white btn-sm btn-primary border-0 mx-1 mb-1 " style="background-color: #FF9391;"><i class="fas fa-trash mx-1"></i>حذف</button><button class="small text-white btn-sm btn-primary border-0 mx-1 mb-1 " style="background-color: #FC7438;"><i class="fas fa-lock mx-1"></i>حظر</button>'
-        ],
-        ["1234", ['<img src="images/user.jpg" width="30px" class="profile_pic rounded-circle mx-1" style="border: 2px solid #fff;">',
-                'احمد عسكر'
-            ],
-            ['<img src="icons/kwait.svg" width="30px" class="profile_pic mx-1">', "+201234567890"],
-            "info@website.com",
-            '<i class="fas fa-check text-success"></i>',
-            '<button class="small text-white btn-sm btn-primary border-0 mx-1 mb-1 " style="background-color: #6788FF;"><i class="fas fa-pen mx-1"></i>تعديل</button><button class="small text-white btn-sm btn-primary border-0 mx-1 mb-1 " style="background-color: #FF9391;"><i class="fas fa-trash mx-1"></i>حذف</button><button class="small text-white btn-sm btn-primary border-0 mx-1 mb-1 " style="background-color: #FC7438;"><i class="fas fa-lock mx-1"></i>حظر</button>'
-        ],
-        ["1234", ['<img src="images/user.jpg" width="30px" class="profile_pic rounded-circle mx-1" style="border: 2px solid #fff;">',
-                'أسامة عسكر'
-            ],
-            ['<img src="icons/kwait.svg" width="30px" class="profile_pic mx-1">', "+201234567890"],
-            "info@website.com",
-            '<i class="fas fa-times text-danger"></i>',
-            '<button class="small text-white btn-sm btn-primary border-0 mx-1 mb-1 " style="background-color: #6788FF;"><i class="fas fa-pen mx-1"></i>تعديل</button><button class="small text-white btn-sm btn-primary border-0 mx-1 mb-1 " style="background-color: #FF9391;"><i class="fas fa-trash mx-1"></i>حذف</button><button class="small text-white btn-sm btn-primary border-0 mx-1 mb-1 " style="background-color: #FC7438;"><i class="fas fa-lock mx-1"></i>حظر</button>'
-        ],
-
-    ]
-    $('#example').DataTable({
-
-        "scrollCollapse": true,
-        "scrollX": true,
-        data: customersData,
-        columns: [
-            { title: "ID" },
-            { title: "اسم العميل" },
-            { title: "رقم الجوال" },
-            { title: "البريد الالكتروني" },
-            { title: "صاحب مشروع" },
-            { title: "" }
-        ],
-        "language": {
-            "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Arabic.json"
-        }
-    });
-
+    // country code 
 
 })
 
