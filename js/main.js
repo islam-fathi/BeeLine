b8f1cbdf43b000a570a23e94137d98115e20ce79
Islam Fathi <46162891+islam-fathi@users.noreply.github.com>
2020-12-05 12:10:25 +0200

    dark mode added and working good

--- js/main.js
+++ js/main.js
@@ -6,52 +6,13 @@ $(document).ready(function() {
         $("#wrapper").toggleClass("toggled");
     });
 
-    // chart 
-    $('.chart').easyPieChart({
-        scaleColor: "#ecf0f1",
-        lineWidth: 10,
-        lineCap: 'butt',
-        barColor: '#51CEC2',
-        trackColor: "#ecf0f1",
-        size: 120,
-        animate: 500
-    });
-
-    $('.chart2').easyPieChart({
-        scaleColor: "#ecf0f1",
-        lineWidth: 10,
-        lineCap: 'butt',
-        barColor: '#F65E5D',
-        trackColor: "#ecf0f1",
-        size: 120,
-        animate: 500
-    });
-    $('.chart3').easyPieChart({
-        scaleColor: "#ecf0f1",
-        lineWidth: 10,
-        lineCap: 'butt',
-        barColor: '#7FD355',
-        trackColor: "#ecf0f1",
-        size: 120,
-        animate: 500
-    });
-
-    // progress animation 
-    $(".meter > span").each(function() {
-        $(this)
-            .data("origWidth", $(this).width())
-            .width(0)
-            .animate({
-                width: $(this).data("origWidth") // or + "%" if fluid
-            }, 1200);
-    });
-
-    // country code 
-
-})
-
-$(document).ready(function() {
+    // pagination text 
     $('#DataTables_Table_0_previous a').html("السابق");
     $('#DataTables_Table_0_next a').html("التالي");
 
+    // dark mode 
+    $('.switch').on('change', function() {
+        console.log('yeah')
+        $('body, h5.title-with-border, .form-holder, nav.navbar.bg-white, table.table, #sidebar-wrapper').toggleClass('dark-mode')
+    })
 })
